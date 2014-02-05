b4b4bdeb85aedc67ed1a5d4030222413f4f1d5d9
Guido Barbaglia <guido.barbaglia@fao.org>
2014-02-05 01:48:14 -0800

    Accordion

--- fenix-D3S-ui/src/main/js/fenix-D3S-client.js
+++ fenix-D3S-ui/src/main/js/fenix-D3S-client.js
@@ -87,18 +87,25 @@ var D3SC = (function() {
                 /* Collect group codes and labels. */
                 var groups = [];
                 var labels = [];
+                var tree = {};
+                var treeLabels = {};
                 for (var i = 0 ; i < json.length ; i++) {
                     if ($.inArray(json[i][0], groups) < 0) {
                         groups.push(json[i][0]);
                         labels.push(json[i][1]);
+                        tree[json[i][0]] = [];
+                        treeLabels[json[i][0]] = [];
                     }
+                    tree[json[i][0]].push(json[i][2]);
+                    treeLabels[json[i][0]].push(json[i][3]);
                 }
 
                 /* Populate the accordion. */
                 for (var i = 0 ; i < groups.length ; i++)
                     addAccordionElement(groups[i], labels[i]);
 
-
+                /* Populate the groups. */
+                populateAccordionGroups(tree, treeLabels);
 
             },
 
@@ -106,6 +113,28 @@ var D3SC = (function() {
 
             }
 
+        });
+
+    };
+
+    function populateAccordionGroups(tree, treeLabels) {
+
+        /* Create the radio buttons. */
+        $.each(tree, function(k, v) {
+            for (var i = 0 ; i < v.length ; i++) {
+                var tmp = $(D3SC.CONFIG.snippets).filter('#radio_element').html();
+                tmp = tmp.replace(new RegExp('radio_id_', 'gm'), ('radio_id_' + v[i]));
+                tmp = tmp.replace(new RegExp('radio_title_', 'gm'), ('radio_title_' + v[i]));
+                $('#radio_buttons_' + k).append(tmp);
+            }
+        });
+
+        /* Add labels to the radio buttons. */
+        $.each(tree, function(k, v) {
+            for (var i = 0 ; i < v.length ; i++) {
+                document.getElementById('radio_title_' + v[i]).innerHTML = treeLabels[k][i];
+                $('#radio_id_' + v[i]).attr('value', v[i]);
+            }
         });
 
     };
