49235f1fd0fb8933e50958ca61fd37f590ff92a5
ivanofenix <ivano.cortesini@gmail.com>
2014-02-07 17:41:40 +0100

    bugfix

--- fenix-D3S-client/src/main/java/org/fao/fenix/client/D3SClient.java
+++ fenix-D3S-client/src/main/java/org/fao/fenix/client/D3SClient.java
@@ -23,14 +23,34 @@ public class D3SClient {
 
 
     //UTILS
-    public <T> T getProxy(Class<T> interfaceClassObj) throws ClassNotFoundException {
+    public String getBasePath() { return basePath; }
+
+    public <T> T getProxy(Class<T> interfaceClassObj, String ... pathPrefix) throws ClassNotFoundException {
         if (clientRest!=null) { //REST PROXY
-            Path pathAnnotation = this.getClass().getAnnotation(Path.class);
-            String path = pathAnnotation!=null ? pathAnnotation.value() : "";
-            return clientRest.target(basePath + (path.length()>0 && path.charAt(0)=='/' ? path.substring(1) : path)).proxy(interfaceClassObj);
+            return clientRest.target(basePath + createServiceBasePath(pathPrefix)).proxy(interfaceClassObj);
         } else { //JMX PROXY
             return null;
         }
+    }
+
+
+    private String createServiceBasePath(String[] pathPrefix) {
+        int trimStart, trimEnd;
+        StringBuilder path = new StringBuilder();
+        if (pathPrefix!=null)
+            for (String p : pathPrefix) {
+                trimStart = p.startsWith("/") ? 1 : 0;
+                trimEnd = p.endsWith("/") ? p.length()-1 : p.length();
+                path.append(p.substring(trimStart,trimEnd)).append('/');
+            }
+        Path pathAnnotation = this.getClass().getAnnotation(Path.class);
+        if (pathAnnotation!=null) {
+            String p = pathAnnotation.value();
+            trimStart = p.startsWith("/") ? 1 : 0;
+            trimEnd = p.endsWith("/") ? p.length()-1 : p.length();
+            path.append(p.substring(trimStart,trimEnd)).append('/');
+        }
+        return path.toString();
     }
 
 
