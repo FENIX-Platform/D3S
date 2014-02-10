ca20152b754014f615222ad6b955ee865ef09ca0
Kalimaha <guido.barbaglia@fao.org>
2014-02-10 15:34:31 +0100

    Building nested fields.

--- fenix-D3S-ui/src/main/js/fenix-D3S-ui.js
+++ fenix-D3S-ui/src/main/js/fenix-D3S-ui.js
@@ -110,45 +110,16 @@ var D3SC = (function() {
     function buildTab(tabID, tabContent) {
         $.each(tabContent, function(k, v) {
             switch (v.TYPE) {
-                case 'STRING'           :   buildString(tabID, k, v);           break;
-                case 'SINGLECHOICE'     :   buildSingleChoice(tabID, k, v);     break;
-                case 'MULTIPLECHOICE'   :   buildMultipleChoice(tabID, k, v);   break;
-                case 'CONTACT'          :   buildContact(tabID, k, v);     break;
-                case 'CONTACTLIST'      :   buildContactList(tabID, k, v);   break;
-                case 'DATE'             :   buildDate(tabID, k, v);             break;
-                case 'TEXTAREA'         :   buildTextArea(tabID, k, v);         break;
-                case 'NESTED'           :   buildNested(tabID, k, v);           break;
+                case 'STRING'           :   buildString(tabID, k, v);                               break;
+                case 'SINGLECHOICE'     :   buildSingleChoice(tabID, k, v);                         break;
+                case 'MULTIPLECHOICE'   :   buildMultipleChoice(tabID, k, v);                       break;
+                case 'CONTACT'          :   buildContact(tabID, k, v);                              break;
+                case 'CONTACTLIST'      :   buildContactList(tabID, k, v);                          break;
+                case 'DATE'             :   buildDate(tabID, k, v);                                 break;
+                case 'TEXTAREA'         :   buildTextArea(tabID, k, v);                             break;
+                case 'NESTED'           :   buildNestedFieldBox(tabID, k, v, 'nested_structure');   break;
             }
         });
-    };
-
-    function buildNested(tabID, id, definition) {
-
-        /* Build HTML. */
-        buildNestedFieldBox(tabID, id, definition, 'nested_structure');
-
-        for (var i = 0 ; i < D3SC.CONFIG.data[id].length ; i++) {
-
-            /* Text area. */
-            CKEDITOR.replace(id + '_nested_description_' + i, {toolbar: 'FAOSTAT'});
-            if (D3SC.CONFIG.data[id][0].description != null) {
-                CKEDITOR.timestamp = (new Date()).toString() ;
-                CKEDITOR.instances[id + '_nested_description_' + i].setData(D3SC.CONFIG.data[id][i].description[D3SC.CONFIG.lang_ISO2]);
-            }
-
-            /* Date */
-            $('#' + id + '_nested_date_' + i).jqxDateTimeInput({height: '33px'});
-            $('#' + id + '_nested_date_' + i).jqxDateTimeInput('setDate', new Date(D3SC.CONFIG.data[id][0].publicationDate));
-
-            /* Title. */
-            $('#' + id + '_nested_title_' + i).val(D3SC.CONFIG.data[id][i].title[D3SC.CONFIG.lang_ISO2]);
-
-            /* Link. */
-            if (D3SC.CONFIG.data[id][i].link != null)
-                $('#' + id + '_nested_link_' + i).val(D3SC.CONFIG.data[id][i].link);
-
-        }
-
     };
 
     function buildTextArea(tabID, id, definition) {
@@ -355,43 +326,90 @@ var D3SC = (function() {
         s = s.replace('_remove_button', id + '_remove_button');
         $('#' + tabID).append(s);
         document.getElementById(id + '_legend').innerHTML = definition[D3SC.CONFIG.lang + '_LABEL']
-        for (var i = 0 ; i < D3SC.CONFIG.data[id].length ; i++) {
-            var tmp = $(D3SC.CONFIG.snippets).filter('#single_nested_structure').html();
-            tmp = tmp.replace('_nested_label_title', id + '_nested_label_title' + '_' + i);
-            tmp = tmp.replace('_nested_label_description', id + '_nested_label_description' + '_' + i);
-            tmp = tmp.replace('_nested_label_date', id + '_nested_label_date' + '_' + i);
-            tmp = tmp.replace('_nested_label_link', id + '_nested_label_link' + '_' + i);
-            tmp = tmp.replace('_nested_title', id + '_nested_title' + '_' + i);
-            tmp = tmp.replace('_nested_description', id + '_nested_description' + '_' + i);
-            tmp = tmp.replace('_nested_date', id + '_nested_date' + '_' + i);
-            tmp = tmp.replace('_nested_link', id + '_nested_link' + '_' + i);
-            tmp = tmp.replace('_help_title', id + '_help_title' + '_' + i);
-            tmp = tmp.replace('_help_description', id + '_help_description' + '_' + i);
-            tmp = tmp.replace('_help_date', id + '_help_date' + '_' + i);
-            tmp = tmp.replace('_help_link', id + '_help_link' + '_' + i);
-            $('#' + id + '_container').append(tmp);
-            document.getElementById(id + '_nested_label_title' + '_' + i).innerHTML = $.i18n.prop('_title');
-            document.getElementById(id + '_nested_label_description' + '_' + i).innerHTML = $.i18n.prop('_description');
-            document.getElementById(id + '_nested_label_date' + '_' + i).innerHTML = $.i18n.prop('_date');
-            document.getElementById(id + '_nested_label_link' + '_' + i).innerHTML = $.i18n.prop('_link');
-            $('#' + id + '_help_title' + '_' + i).attr('title', definition.FIELDS.title[D3SC.CONFIG.lang + '_DESCRIPTION']);
-            if (definition.FIELDS.date != null)
-                $('#' + id + '_help_date' + '_' + i).attr('title', definition.FIELDS.date[D3SC.CONFIG.lang + '_DESCRIPTION']);
-            if (definition.FIELDS.link != null)
-                $('#' + id + '_help_link' + '_' + i).attr('title', definition.FIELDS.link[D3SC.CONFIG.lang + '_DESCRIPTION']);
-            if (definition.FIELDS.description != null)
-                $('#' + id + '_help_description' + '_' + i).attr('title', definition.FIELDS.description[D3SC.CONFIG.lang + '_DESCRIPTION']);
-        }
+        for (var i = 0 ; i < D3SC.CONFIG.data[id].length ; i++)
+            addNestedFieldBox(id, definition, i);
         document.getElementById(id + '_add_button').innerHTML = $.i18n.prop('_add') + ' ' + definition[D3SC.CONFIG.lang + '_TAB_LABEL'];
         document.getElementById(id + '_remove_button').innerHTML = $.i18n.prop('_remove') + ' ' + definition[D3SC.CONFIG.lang + '_TAB_LABEL'];
         $('#' + id + '_add_button').bind('click', function() {
-            D3SC.addNestedFieldBox(id);
+            var index = findMaxNested(id);
+            D3SC.addNestedFieldBox(id, definition, index);
+        });
+        $('#' + id + '_remove_button').bind('click', function() {
+            var index = findMaxNested(id);
+            D3SC.removeNestedFieldBox(id, index);
         });
     };
 
-    function addNestedFieldBox(id) {
-        var s = $(D3SC.CONFIG.snippets).filter('#single_nested_structure').html();
-        console.log(s);
+    function findMaxNested(id) {
+        return $('[id^="' + id + '_single_nested_"]').length;
+    };
+
+    function removeNestedFieldBox(id, index) {
+        if ((index - 1) > 0)
+            $('#' + id + '_single_nested_' + (index - 1)).remove();
+    };
+
+    function addNestedFieldBox(id, definition, index) {
+
+        /* Build HTML. */
+        var tmp = $(D3SC.CONFIG.snippets).filter('#single_nested_structure').html();
+        tmp = tmp.replace('_nested_label_title', id + '_nested_label_title' + '_' + index);
+        tmp = tmp.replace('_nested_label_description', id + '_nested_label_description' + '_' + index);
+        tmp = tmp.replace('_nested_label_date', id + '_nested_label_date' + '_' + index);
+        tmp = tmp.replace('_nested_label_link', id + '_nested_label_link' + '_' + index);
+        tmp = tmp.replace('_nested_title', id + '_nested_title' + '_' + index);
+        tmp = tmp.replace('_nested_description', id + '_nested_description' + '_' + index);
+        tmp = tmp.replace('_nested_date', id + '_nested_date' + '_' + index);
+        tmp = tmp.replace('_nested_link', id + '_nested_link' + '_' + index);
+        tmp = tmp.replace('_help_title', id + '_help_title' + '_' + index);
+        tmp = tmp.replace('_help_description', id + '_help_description' + '_' + index);
+        tmp = tmp.replace('_help_date', id + '_help_date' + '_' + index);
+        tmp = tmp.replace('_help_link', id + '_help_link' + '_' + index);
+        tmp = tmp.replace('_single_nested', id + '_single_nested' + '_' + index);
+        $('#' + id + '_container').append(tmp);
+        document.getElementById(id + '_nested_label_title' + '_' + index).innerHTML = $.i18n.prop('_title');
+        document.getElementById(id + '_nested_label_description' + '_' + index).innerHTML = $.i18n.prop('_description');
+        document.getElementById(id + '_nested_label_date' + '_' + index).innerHTML = $.i18n.prop('_date');
+        document.getElementById(id + '_nested_label_link' + '_' + index).innerHTML = $.i18n.prop('_link');
+        $('#' + id + '_help_title' + '_' + index).attr('title', definition.FIELDS.title[D3SC.CONFIG.lang + '_DESCRIPTION']);
+        if (definition.FIELDS.date != null)
+            $('#' + id + '_help_date' + '_' + index).attr('title', definition.FIELDS.date[D3SC.CONFIG.lang + '_DESCRIPTION']);
+        if (definition.FIELDS.link != null)
+            $('#' + id + '_help_link' + '_' + index).attr('title', definition.FIELDS.link[D3SC.CONFIG.lang + '_DESCRIPTION']);
+        if (definition.FIELDS.description != null)
+            $('#' + id + '_help_description' + '_' + index).attr('title', definition.FIELDS.description[D3SC.CONFIG.lang + '_DESCRIPTION']);
+
+        /* Initiate components. */
+        for (var i = 0 ; i <= index ; i++) {
+            try {
+                CKEDITOR.replace(id + '_nested_description_' + i, {toolbar: 'FAOSTAT'});
+            } catch (e) {
+
+            }
+            $('#' + id + '_nested_date_' + i).jqxDateTimeInput({height: '33px'});
+        }
+
+        /* Populate interface. */
+        for (var i = 0 ; i < D3SC.CONFIG.data[id].length ; i++) {
+
+            /* Text area. */
+            if (D3SC.CONFIG.data[id][0].description != null) {
+                CKEDITOR.timestamp = (new Date()).toString() ;
+                CKEDITOR.instances[id + '_nested_description_' + i].setData(D3SC.CONFIG.data[id][i].description[D3SC.CONFIG.lang_ISO2]);
+            }
+
+            /* Date */
+            $('#' + id + '_nested_date_' + i).jqxDateTimeInput('setDate', new Date(D3SC.CONFIG.data[id][i].publicationDate));
+
+            /* Title. */
+            $('#' + id + '_nested_title_' + i).val(D3SC.CONFIG.data[id][i].title[D3SC.CONFIG.lang_ISO2]);
+
+            /* Link. */
+            if (D3SC.CONFIG.data[id][i].link != null)
+                $('#' + id + '_nested_link_' + i).val(D3SC.CONFIG.data[id][i].link);
+
+        }
+
     };
 
     function loadMSD() {
@@ -450,9 +468,10 @@ var D3SC = (function() {
     };
 
     return {
-        init                :   init,
-        addNestedFieldBox   :   addNestedFieldBox,
-        CONFIG              :   CONFIG
+        init                    :   init,
+        addNestedFieldBox       :   addNestedFieldBox,
+        removeNestedFieldBox    :   removeNestedFieldBox,
+        CONFIG                  :   CONFIG
     };
 
 })();
