e6da3841a53caf8ee45452a1867f99d794e5b294
ivanofenix <ivano.cortesini@gmail.com>
2014-04-14 10:57:28 +0200

    ...

--- fenix-D3S-proxy/src/main/java/org/fao/fenix/d3s/server/services/rest/ServiceRegistry.java
+++ fenix-D3S-proxy/src/main/java/org/fao/fenix/d3s/server/services/rest/ServiceRegistry.java
@@ -3,6 +3,6 @@ package org.fao.fenix.d3s.server.services.rest;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-@ApplicationPath("/")
+@ApplicationPath("/service")
 public class ServiceRegistry extends Application {
 }
