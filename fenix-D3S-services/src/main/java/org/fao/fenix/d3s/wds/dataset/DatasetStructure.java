740dbd9173bd0374e6a875c64cef8350e220f425
Ivano Cortesini <ivano.cortesini@gmail.com>
2014-09-18 17:29:24 +0200

    ...

--- fenix-D3S-services/src/main/java/org/fao/fenix/d3s/wds/dataset/DatasetStructure.java
+++ fenix-D3S-services/src/main/java/org/fao/fenix/d3s/wds/dataset/DatasetStructure.java
@@ -52,12 +52,13 @@ public class DatasetStructure {
 
     public DSDColumn[] columns;
 
-    public DSDColumn[] selectColumns;
     public Object[] singleValues;
-    public String selectColumnsQueryFields;
-
-    public Integer[] selectColumnsIndexes;
-    public Integer[] keyColumnsIndexes;
     public Integer[] singleValuesIndexes;
+
+    public DSDColumn[] selectColumns;
+    public String selectColumnsQueryFields;
+    public Integer[] selectColumnsIndexes;
+
+    public Integer[] keyColumnsIndexes;
 
 }
