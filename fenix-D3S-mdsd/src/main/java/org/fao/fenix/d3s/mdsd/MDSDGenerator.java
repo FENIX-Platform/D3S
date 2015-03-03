25905c1bedd2832c682f9c2b0db277d5fccce290
Kalimaha <guido.barbaglia@gmail.com>
2015-03-03 14:16:36 +0100

    MDSD generator.

--- fenix-D3S-mdsd/src/main/java/org/fao/fenix/d3s/mdsd/MDSDGenerator.java
+++ fenix-D3S-mdsd/src/main/java/org/fao/fenix/d3s/mdsd/MDSDGenerator.java
@@ -1,5 +1,9 @@
 package org.fao.fenix.d3s.mdsd;
 
+/**
+ * @author <a href="mailto:guido.barbaglia@fao.org">Guido Barbaglia</a>
+ * @author <a href="mailto:guido.barbaglia@gmail.com">Guido Barbaglia</a>
+ */
 public class MDSDGenerator {
 
 }
--- fenix-D3S-mdsd/src/test/java/org/fao/fenix/d3s/mdsd/MDSDGeneratorTest.java
+++ fenix-D3S-mdsd/src/test/java/org/fao/fenix/d3s/mdsd/MDSDGeneratorTest.java
@@ -2,6 +2,10 @@ package org.fao.fenix.d3s.mdsd;
 
 import junit.framework.TestCase;
 
+/**
+ * @author <a href="mailto:guido.barbaglia@fao.org">Guido Barbaglia</a>
+ * @author <a href="mailto:guido.barbaglia@gmail.com">Guido Barbaglia</a>
+ */
 public class MDSDGeneratorTest extends TestCase {
 
 }
