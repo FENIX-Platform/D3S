d629c554604ed375a9f8519dd643e775b7d5fadf
Kalimaha <guido.barbaglia@gmail.com>
2015-03-04 16:20:26 +0100

    Add children

--- fenix-D3S-mdsd/src/main/java/org/fao/fenix/d3s/mdsd/MDSDGenerator.java
+++ fenix-D3S-mdsd/src/main/java/org/fao/fenix/d3s/mdsd/MDSDGenerator.java
@@ -2,9 +2,11 @@ package org.fao.fenix.d3s.mdsd;
 
 import org.fao.fenix.commons.annotations.Description;
 import org.fao.fenix.commons.annotations.Label;
+import org.fao.fenix.commons.msd.dto.JSONEntity;
 import org.fao.fenix.commons.msd.dto.full.MeIdentification;
 
 import java.lang.annotation.Annotation;
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.ParameterizedType;
 
@@ -18,47 +20,94 @@ public class MDSDGenerator {
 
         /* Initiate output. */
         StringBuilder sb = new StringBuilder();
-        sb.append("{").append("\"type\": \"object\",\"properties\": {");
 
+        /* Root object. */
         MeIdentification obj = new MeIdentification();
-        for (Field f : obj.getClass().getDeclaredFields()) {
-//            System.out.println(f.getName() + " - " + f.getType().getSimpleName());
-            try {
-                Label l = (Label)f.getDeclaredAnnotation(Label.class);
-                Description d = (Description)f.getDeclaredAnnotation(Description.class);
-                System.out.println(f.getType().getSimpleName());
-                if (f.getType().getSimpleName().equalsIgnoreCase("Collection")) {
-                    ParameterizedType stringListType = (ParameterizedType) f.getGenericType();
-                    Class<?> stringListClass = (Class<?>) stringListType.getActualTypeArguments()[0];
-                    System.out.println("\t" + stringListClass.getSimpleName());
-                }
-                sb.append(encodeField(f.getName(), f.getType().getSimpleName(), l, d));
-                sb.append(",");
-            } catch (Exception e) {
 
-            }
-        }
-        sb.deleteCharAt(sb.length() - 1);
+        /* Process D3S domain. */
+        sb.append(processObject(obj));
 
         /* Return output. */
-        sb.append("}}");
         System.out.println(sb);
         return sb.toString();
 
     }
 
-    private StringBuilder encodeField(String fieldName, String fieldType, Label l, Description d) {
+    private StringBuilder processObject(Object obj) {
+        System.out.println("PROCESSING " + obj.getClass().getSimpleName());
         StringBuilder sb = new StringBuilder();
-        sb.append("\"").append(fieldName).append("\": {");
-        sb.append("\"type\": \"").append(fieldType).append("\",\"properties\": {");
+        sb.append("{");
+        sb.append("\"type\": \"object\",");
+        sb.append("\"properties\": {");
+        for (int i = 0 ; i < obj.getClass().getDeclaredFields().length ; i++) {
+            Field f = obj.getClass().getDeclaredFields()[i];
+//            System.out.println(f.getName() + " - " + f.getType().getSimpleName());
+            try {
+                Label l = (Label)f.getDeclaredAnnotation(Label.class);
+                Description d = (Description)f.getDeclaredAnnotation(Description.class);
+//                System.out.println(f.getType().getSimpleName());
+                sb.append(encodeField(f, l, d));
+
+//                if (f.getType().getSimpleName().equalsIgnoreCase("Collection")) {
+//                    ParameterizedType stringListType = (ParameterizedType) f.getGenericType();
+//                    Class<?> stringListClass = (Class<?>) stringListType.getActualTypeArguments()[0];
+//                    System.out.println("\t" + stringListClass.getSimpleName());
+//                } else {
+//                try {
+//                    System.out.println(f.getName() + ", " + f.getType().getSimpleName());
+//                    Object o = Class.forName(f.getType().getCanonicalName()).newInstance();
+//                    if (!(o instanceof String)) {
+//                        System.out.println("\tAdd sub-objects");
+//                        sb.append(processObject(o));
+//                    }
+//                } catch (Exception e) {
+//
+//                }
+//                    sb.append("{").append("\"type\": \"object\",\"properties\": {");
+//                    sb.append(processObject(o));
+//                    sb.append("}");
+//                }
+
+                sb.append(",");
+            } catch (Exception e) {
+            }
+        }
+        sb.deleteCharAt(sb.length() - 1);
+        sb.append("}");
+        sb.append("}");
+        return sb;
+    }
+
+    private StringBuilder encodeField(Field f, Label l, Description d) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("\"").append(f.getName()).append("\": {");
+        sb.append("\"type\": \"").append(f.getType().getSimpleName()).append("\",");
+        sb.append("\"properties\": {");
 
         sb.append(encodeLabel(l));
         sb.append(",");
         sb.append(encodeDescription(d));
 
+        try {
+            System.out.println(f.getName() + ", " + f.getType().getSimpleName());
+            Object o = Class.forName(f.getType().getCanonicalName()).newInstance();
+            if (!(o instanceof String)) {
+                sb.append(",");
+                System.out.println("\tAdd sub-objects");
+                sb.append(processObject(o));
+            }
+        } catch (Exception e) {
+
+        }
+
 
         sb.append("}");
         sb.append("}");
+        return sb;
+    }
+
+    private StringBuilder encodeString() {
+        StringBuilder sb = new StringBuilder();
         return sb;
     }
 
