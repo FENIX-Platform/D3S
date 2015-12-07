b4f3c0a010d8d1b9d11539a32642c906dc40fef6
Ivano Cortesini <ivano.cortesini@gmail.com>
2015-12-07 17:24:25 +0100

    ...

--- fenix-D3S-services/src/main/java/org/fao/fenix/d3s/msd/listener/MetadataListenerFactory.java
+++ fenix-D3S-services/src/main/java/org/fao/fenix/d3s/msd/listener/MetadataListenerFactory.java
@@ -2,15 +2,17 @@ package org.fao.fenix.d3s.msd.listener;
 
 import org.fao.fenix.commons.msd.dto.full.MeIdentification;
 
+import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Instance;
 import javax.inject.Inject;
 import java.util.*;
 
+@ApplicationScoped
 public class MetadataListenerFactory {
     @Inject private Instance<MetadataListener> listenerInstanceProducer;
+    private Map<String, Set<Class>> contextListenersCache = new HashMap<>();
 
-
-    //Retrieve cache listeners
+    //Retrieve metadata listeners
     public Collection<MetadataListener> getListeners(String context) {
         Collection<MetadataListener> list = new LinkedList<>();
         MetadataListener instance;
@@ -22,10 +24,13 @@ public class MetadataListenerFactory {
     }
 
     private boolean validateContext(String context, MetadataListener listener) {
-        Context contextAnnotation = listener.getClass().getAnnotation(Context.class);
-        String[] pluginContexts = contextAnnotation!=null ? contextAnnotation.value() : null;
-        Set<String> pluginContextsSet = pluginContexts!=null ? new HashSet<>(Arrays.asList(pluginContexts)) : null;
-        return pluginContextsSet==null || pluginContextsSet.contains(context);
+        if (context!=null) {
+            Context contextAnnotation = listener.getClass().getAnnotation(Context.class);
+            String[] pluginContexts = contextAnnotation != null ? contextAnnotation.value() : null;
+            Set<String> pluginContextsSet = pluginContexts != null ? new HashSet<>(Arrays.asList(pluginContexts)) : null;
+            return pluginContextsSet == null || pluginContextsSet.contains(context);
+        } else
+            return false;
     }
 
 }
