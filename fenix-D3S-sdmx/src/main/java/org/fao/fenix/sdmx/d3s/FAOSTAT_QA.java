898d46e4c9467e9343e618504b7ff1f340143716
joyeux <joyeux@ESS328.hq.un.fao.org>
2015-12-11 10:34:20 +0100

    pivotUI work

--- fenix-D3S-sdmx/src/main/java/org/fao/fenix/sdmx/d3s/FAOSTAT_QA.java
+++ fenix-D3S-sdmx/src/main/java/org/fao/fenix/sdmx/d3s/FAOSTAT_QA.java
@@ -7,6 +7,9 @@ import org.fao.fenix.commons.msd.dto.full.DSDColumn;
 import org.fao.fenix.commons.msd.dto.full.DSDDataset;
 import org.fao.fenix.commons.msd.dto.type.DataType;
 
+import demo.sdmxsource.webservice.main.finalPackage.ExportSDMX;
+import java.io.OutputStream;
+
 import java.util.Collection;
 import java.util.LinkedList;
 
@@ -18,9 +21,17 @@ public class FAOSTAT_QA {
         //Load codelists
         Collection<Resource<DSDCodelist,Code>> codeLists = new LinkedList<>();
         for (DSDColumn column : dataset.getMetadata().getDsd().getColumns())
-            if (column.getDataType()== DataType.code)
+        {
+            if (column.getDataType()== DataType.code){
                 codeLists.add(ResourceFactory.getCodelistInstance(column.getDomain().getCodes().iterator().next().getIdCodeList()));
-        //TODO do something to translate to SDMX
+            }
+        }
+                //TODO do something to translate to SDMX
+        ExportSDMX test=new ExportSDMX();
+       test.execution(dataset,codeLists);
+       //test.
+     OutputStream Object1 = test.getDatafile();
+      OutputStream  Object2=test.getStructurefile();
         System.out.println("All done...");
     }
 }
--- fenix-D3S-sdmx/src/main/java/org/fao/fenix/sdmx/d3s/FAOSTAT_QA_label.java
+++ fenix-D3S-sdmx/src/main/java/org/fao/fenix/sdmx/d3s/FAOSTAT_QA_label.java
@@ -1,5 +1,6 @@
 package org.fao.fenix.sdmx.d3s;
 
+//import demo.sdmxsource.webservice.main.chapter1.Chapter1WritingStructures;
 import org.fao.fenix.commons.msd.dto.data.Resource;
 import org.fao.fenix.commons.msd.dto.full.Code;
 import org.fao.fenix.commons.msd.dto.full.DSDCodelist;
@@ -16,6 +17,8 @@ public class FAOSTAT_QA_label {
         //Load dataset
         Resource<DSDDataset, Object[]> dataset = ResourceFactory.getDatasetInstance("FAOSTAT_QA_label");
         //TODO do something to translate to SDMX
+      //   Chapter1WritingStructures test=new Chapter1WritingStructures();
+      //  test.execution(dataset);
         System.out.println("All done...");
     }
 }
