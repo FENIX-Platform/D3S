e63b35d44f79d4c34873bfd1f66f90d45989779f
fabriziofenix <fabrizio.castelli@fao.org>
2016-05-04 18:19:08 +0200

    export processes

--- fenix-D3S-export/src/main/java/org/fao/fenix/export/d3s/D3SDatasetInputPlugin.java
+++ fenix-D3S-export/src/main/java/org/fao/fenix/export/d3s/D3SDatasetInputPlugin.java
@@ -5,8 +5,10 @@ import org.fao.fenix.commons.msd.dto.full.DSDDataset;
 import org.fao.fenix.commons.msd.dto.full.MeIdentification;
 import org.fao.fenix.d3s.msd.services.rest.ResourcesService;
 import org.fao.fenix.export.core.dto.data.CoreData;
+import org.fao.fenix.export.core.dto.data.CoreTableData;
 import org.fao.fenix.export.core.input.plugin.Input;
 
+import java.util.Collection;
 import java.util.Iterator;
 import java.util.Map;
 
@@ -14,10 +16,12 @@ public class D3SDatasetInputPlugin extends Input {
     String uid;
     String version;
     ResourcesService service;
+    Resource<DSDDataset,Object[]> resource;
 
     @Override
     public void init(Map<String, Object> config, Resource resource) {
-        MeIdentification metadata = resource!=null ? resource.getMetadata() : null;
+        this.resource = resource;
+        MeIdentification<DSDDataset> metadata = resource!=null ? (MeIdentification<DSDDataset>) resource.getMetadata() : null;
         uid = metadata!=null ? metadata.getUid() : null;
         version = metadata!=null ? metadata.getVersion() : null;
         service = ExportManager.getResourcesService(config!=null && config.containsKey("lang") ? (String)config.get("lang") : "EN");
@@ -25,24 +29,38 @@ public class D3SDatasetInputPlugin extends Input {
 
     @Override
     public CoreData getResource() {
-        try {
-            long time = System.currentTimeMillis();
-            final Resource<DSDDataset,Object[]> data = service.loadResource(uid, version);
-            System.out.println("Time = "+(System.currentTimeMillis()-time));
-            if (data!=null)
-                return new CoreData<Object[]>() {
-                    @Override
-                    public MeIdentification getMetadata() {
-                        return data.getMetadata();
-                    }
-                    @Override
-                    public Iterator<Object[]> getData() {
-                        return data.getData()!=null ? data.getData().iterator() : null;
-                    }
-                };
-        } catch (Exception e) {
-            e.printStackTrace();
+        if(!isResourceFull()) {
+            try {
+                long time = System.currentTimeMillis();
+                final Resource<DSDDataset, Object[]> data = service.loadResource(uid, version);
+                System.out.println("Time = " + (System.currentTimeMillis() - time));
+                if (data != null)
+                    return new CoreData<Object[]>() {
+                        @Override
+                        public MeIdentification getMetadata() {
+                            return data.getMetadata();
+                        }
+
+                        @Override
+                        public Iterator<Object[]> getData() {
+                            return data.getData() != null ? data.getData().iterator() : null;
+                        }
+                    };
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }else {
+           return new CoreTableData(this.resource.getMetadata(),this.resource.getData().iterator());
         }
         return null;
+
+    }
+
+
+    private boolean isResourceFull() {
+        return resource!= null && resource.getMetadata()!= null && resource.getMetadata().getDsd()!= null
+                && (resource.getMetadata().getDsd()).getColumns()!= null
+                &&(resource.getMetadata().getDsd()).getColumns().size()>0;
+
     }
 }
